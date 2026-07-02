f48fb6f301039a9e5de7a8fe5f66828e925aa152
Edison <edisonsbrown@gmail.com>
2026-07-02 10:50:45 -0400

    Fixed file referencing and root directory

--- Kirby_detector.py
+++ Kirby_detector.py
@@ -1,43 +1,56 @@
-import cv2
 import os
+import cv2
 
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+root_dir = r"C:\Users\ediso\Downloads\Python Projects\Kirby project\kirby_images" # Raw string
+target_extension = ".png"
 
 
 
 def Image_folder_processing():
     file_templates = []
-    for file in os.listdir("Kirby_images"):
-        if file.endswith(".png"): # Only pulling png images
+    for root, dirs, files in os.walk(root_dir): # os.walk will go into subfolders to find every target within the directory
+        for file in files:
+            if file.endswith(target_extension): # Only pulling png images
 
-            image = cv2.imread(os.path.join("Kirby_images", file), 0)
+                image = cv2.imread(os.path.join(root_dir, file), 1) # 1 allows them to load in color
 
-            file_templates.append(image)
+                file_templates.append(image)
 
     return file_templates
 
-while rval:
-    cv2.imshow("preview", frame)
+def Image_pattern_scoring():
+    print("")
+
+
+rval = True
+
+if vc.isOpened(): # try to get the first frame of camera
     rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
+
+    for file in Image_folder_processing():
+        cv2.imshow(f"file:{file}", file)
+
+    while rval:
+
+        
+       
 
 
 
+        cv2.imshow("preview", frame)
+        rval, frame = vc.read()
 
 
 
+        key = cv2.waitKey(20)
+        if key == 27: # exit on ESC
+            break
 
+    cv2.destroyWindow("preview")
+    vc.release()
 
-
-
-
-cv2.destroyWindow("preview")
-vc.release()
+else:
+    rval = False
